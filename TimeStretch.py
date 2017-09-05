a38623c3e1046d728ee3e85f79c94aded9830b54
alex walters <tritium@sdamon.com>
2017-09-05 12:34:32 -0400

    Refactor

--- TimeStretch.py
+++ TimeStretch.py
@@ -23,15 +23,28 @@ def _format_time(total_seconds):
     minutes, seconds = divmod(minute_second, 60)
     return "{}:{:0>2}:{:0>2}".format(hours, minutes, seconds)
 
-
-class GUI:
-    def __init__(self, root):
-        self.root = root
+class App:
+    def __init__(self, root=None):
         self.stretch_factor = 1.0
         self.total_seconds = 3600
-
+        if root is None:
+            self.root = tkinter.Tk()
+            self.init_root()
+        else:
+            self.root = root
         self.init_vars()
         self.init_ui()
+
+    def init_root(self):
+        self.root.columnconfigure(0, weight=1)
+        self.root.resizable(0, 0)
+        self.root.minsize(width=300, height=0)
+        self.root.title("Time Stretch Calculator")
+        if getattr(sys, 'frozen', False):
+            icon = path.join(sys._MEIPASS, 'clock.ico')
+        else:
+            icon = path.join(path.dirname(path.abspath(__file__)), 'clock.ico')
+        self.root.iconbitmap(icon)
 
     def init_vars(self):
         self.input_var = tkinter.StringVar()
@@ -45,40 +58,20 @@ class GUI:
         self.stretch_var.set(self.stretch_factor)
 
     def init_ui(self):
-        self.root.columnconfigure(0, weight=1)
-        self.root.resizable(0, 0)
-        self.root.title("Time Stretch Calculator")
-        self.set_icon()
-        self.root.minsize(width=300, height=0)
         self.mainframe = ttk.Frame(self.root)
         self.mainframe.grid(column=0, row=0, sticky="NEWS")
         self.mainframe.columnconfigure(0, weight=1)
         self.mainframe.columnconfigure(1, weight=0)
-
         self.input = ttk.Entry(self.mainframe, textvariable=self.input_var)
         self.input.grid(column=0, row=0, sticky="EW", columnspan=2)
-
         self.stretch = ttk.Scale(self.mainframe, from_=0.1, to=3.0, variable=self.stretch_var)
         self.stretch.grid(column=0, row=1, sticky="EW")
-
         self.stretch_label = ttk.Label(self.mainframe, textvariable=self.stretch_label_var)
         self.stretch_label.grid(column=1, row=1, sticky="EW")
-
         self.output = ttk.Label(self.mainframe, textvariable=self.output_var)
         self.output.grid(column=0, row=2, sticky="EW", columnspan=2)
-
         self.finish = ttk.Label(self.mainframe, textvariable=self.finish_var)
         self.finish.grid(column=0, row=3, sticky="EW", columnspan=2)
-
-    def set_icon(self):
-        if getattr(sys, 'frozen', False):
-            icon = path.join(sys._MEIPASS, 'clock.ico')
-        else:
-            icon = path.join(
-                path.dirname(path.abspath(__file__)),
-                'clock.ico',
-            )
-        self.root.iconbitmap(icon)
 
     def _trace_stretch(self, *args):
         val = self.stretch_var.get()
@@ -98,7 +91,8 @@ class GUI:
         finish = datetime.datetime.now() + td
         self.finish_var.set('Finished at:\t' + finish.strftime('%x %X'))
 
+    def mainloop(self):
+        self.root.mainloop()
 
-root = tkinter.Tk()
-gui = GUI(root)
-root.mainloop()
+if __name__ == '__main__':
+    App().mainloop()
